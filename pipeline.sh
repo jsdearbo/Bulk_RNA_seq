2924775f67ee8bf97b13386e3333e1515f7d7693
jsdearbo <jakedeaborn@gmail.com>
2024-06-08 02:49:12 -0400

    created wrapper script for user input

--- pipeline.sh
+++ pipeline.sh
@@ -2,77 +2,79 @@
 
 # ensure that you have a folder with your experiment name
 # within that folder must be a fastq folder that contains your gz compressed fastq files
-    # possible update: nest in an if loop fastq or fastq gz compatabliity
-# Set the directory containing the files. starts w/gz files
 
-echo "Please enter the directory path to the fastq folder within your dedicated experiment folder:"
+# Set the directory containing the files. starts w/gz files
+echo "Please enter the directory path to the fastq directory within your dedicated experiment directory:"
 read -e -p "directory path: " file_path
+
+PIPELINE_LAUNCH="/bobross/jdearborn/shell_scripts/Bulk_RNA_seq/pipeline/run_pipeline.sh"
 
 if [ -d "$file_path" ]; then
     echo "The directory path is: $file_path"
-    # Perform operations with the directory path
+    INPUT_DIR="$file_path" #use for rename, fastqc
+    EXPERIMENT_DIR=$(dirname "$file_path")  # pass this to scripts
+    echo "EXPERIMENT_DIR: $EXPERIMENT_DIR"
+    
+    # Prompt the user to enter a species for alignment and feature counts
+    while true; do
+        echo "Alignment to mouse or human genome?"
+        read -e -p "Type m or h and press enter: " species
+
+        case "$species" in
+            m|M)
+                GENOME="mouse"
+                break
+                ;;
+            h|H)
+                GENOME="human"
+                break
+                ;;
+            *)
+                echo "Invalid input. Please try again."
+                ;;
+        esac
+    done
+    echo "Genome selected: $GENOME"
+
+    # Prompt the user to run fastqc or not
+    while true; do
+        echo "Do you want to run FastQC on your FASTQ files? (adds ~10 min/file)"
+        read -e -p "Type y or n and press enter: " QC_CHECK
+
+        case "$QC_CHECK" in
+            y|Y)
+                RUN_QC="yes"
+                break
+                ;;
+            n|N)
+                RUN_QC="no"
+                break
+                ;;
+            *)
+                echo "Invalid input. Please type 'y' or 'n'."
+                ;;
+        esac
+    done
+    # Set and create a logs directories
+    LOG_DIR="$EXPERIMENT_DIR/pipeline_logs"
+    mkdir -p "$LOG_DIR"
+
+    # Run the pipeline function with nohup
+    nohup bash "$PIPELINE_LAUNCH" "$RUN_QC" "$GENOME" "$EXPERIMENT_DIR" "$LOG_DIR" > "$LOG_DIR/pipeline.log" 2>&1 
+    #nohup bash -c '. "./run_pipeline.sh"; run_pipeline "$RUN_QC" "$GENOME" "$EXPERIMENT_DIR"' > "$LOG_DIR/pipeline.log" 2>&1 &
 else
     echo "Error: The provided path is not a valid directory."
     exit 1
 fi
 
-INPUT_DIR="$file_path" #use for rename, fastqc
-EXPERIMENT_DIR=$(dirname "$file_path")  # pass this to scripts
-echo "EXPERIMENT_DIR: $EXPERIMENT_DIR"
-
-# Prompt the user to enter a species for alignment and feature counts
-while true; do
-    echo "Alignment to mouse or human genome?"
-    read -e -p "Type m or h and press enter: " species
-
-    case "$species" in
-        m)
-            GENOME="mouse"
-            break
-            ;;
-        h)
-            GENOME="human"
-            break
-            ;;
-        *)
-            echo "Invalid input. Please try again."
-            ;;
-    esac
-done
-
-echo "Genome selected: $GENOME"
-
-# Set the paths to the scripts
-RENAME_SCRIPT="/bobross/jdearborn/shell_scripts/Bulk_RNA_seq/pipeline/rename_fastqs.sh" # removes lane number from sample name
-FASTQC_SCRIPT="/bobross/jdearborn/shell_scripts/Bulk_RNA_seq/pipeline/run_fastqc.sh" # QC assessment
-STAR_LOOP_SCRIPT="/bobross/jdearborn/shell_scripts/Bulk_RNA_seq/pipeline/star_loop.sh" # Alignment loop
-FEATURECOUNTS_SCRIPT="/bobross/jdearborn/shell_scripts/Bulk_RNA_seq/pipeline/run_featureCounts_batched.sh" # feature counts batched
-
-# START CALLING SCRIPTS
-# Run the rename_fastqs.sh script. Also used for decompression .gz files
-echo "Running rename_fastqs.sh..."
-bash "$RENAME_SCRIPT" "$EXPERIMENT_DIR"
-
-# Run FastQC
-echo "Running run_fastqc.sh..."
-"$FASTQC_SCRIPT" "$EXPERIMENT_DIR"
-
-# # Run the star_loop.sh script
-echo "Running star_loop.sh..."
-"$STAR_LOOP_SCRIPT" "$EXPERIMENT_DIR" "$GENOME"
-
-# Run the run_featureCounts_batched.sh script
-echo "Running run_featureCounts_batched.sh..."
-"$FEATURECOUNTS_SCRIPT" "$EXPERIMENT_DIR" "$GENOME"
-
 
 # improvements to the pipeline:
-# -prompt species for alignment/annotation
-    # -encode the input of this prompt to a file path for star/featurecounts references
-    # -pass this as a 2nd argument to star and feature counts script calls
-# -fastqc y/n and other software prompts
+# file finder outputs for logs as with featurecounts
+    # this is a nice feature
 # -sequencing vendor specifics, we're set up for OMRF nomanclature here
+    #fastq or fq accommadation
 # -option to run featurecounts batched or looped (low priority)
 # -add an automated call to gzip input fastqs. this will need to be carefully implemented as it must check completion first.
     # -frankly not sure if this is in the cards
 # -add prompts for user input component selections: do yo want to run fastqc?
+# find a place for log files
---
+++ run_pipeline.sh
@@ -0,0 +1,45 @@
+# Carry in user input variables. 
+# This is necessary to run the whole pipeline via a nested nohup call.
+RUN_QC="$1"
+GENOME="$2"
+EXPERIMENT_DIR="$3"
+LOG_DIR="$4"
+
+# Set the paths to the scripts
+RENAME_SCRIPT="/bobross/jdearborn/shell_scripts/Bulk_RNA_seq/pipeline/rename_fastqs.sh" # removes lane number from sample name
+FASTQC_SCRIPT="/bobross/jdearborn/shell_scripts/Bulk_RNA_seq/pipeline/run_fastqc.sh" # QC assessment
+STAR_LOOP_SCRIPT="/bobross/jdearborn/shell_scripts/Bulk_RNA_seq/pipeline/star_loop.sh" # Alignment loop
+FEATURECOUNTS_SCRIPT="/bobross/jdearborn/shell_scripts/Bulk_RNA_seq/pipeline/run_featureCounts_batched.sh" # feature counts batched
+
+# Set log paths (integrate this?)
+RENAME_LOG="$LOG_DIR/rename_fastqs.log"
+FASTQC_LOG="$LOG_DIR/run_fastqc.log"
+STAR_LOG="$LOG_DIR/star_loop.log"
+FEATURECOUNTS_LOG="$LOG_DIR/run_featureCounts.log"
+
+touch "$RENAME_LOG"
+
+# Run the rename_fastqs.sh script. Also used for decompression of .gz files
+echo "Running rename_fastqs.sh..."
+nohup bash "$RENAME_SCRIPT" "$EXPERIMENT_DIR" > "$RENAME_LOG" 2>&1
+echo "rename_fastqs.sh completed."
+
+# Run FastQC
+if [ "$RUN_QC" = "yes" ]; then
+    echo "Running run_fastqc.sh..."
+    nohup bash "$FASTQC_SCRIPT" "$EXPERIMENT_DIR" > "$FASTQC_LOG" 2>&1
+    echo "run_fastqc.sh completed."
+else
+    echo "Skipping FastQC run."
+fi
+
+# Run the star_loop.sh script
+echo "Running star_loop.sh..."
+nohup bash "$STAR_LOOP_SCRIPT" "$EXPERIMENT_DIR" "$GENOME" > "$STAR_LOG" 2>&1
+echo "star_loop.sh completed."
+
+# Run the run_featureCounts_batched.sh script
+echo "Running run_featureCounts_batched.sh..."
+nohup bash "$FEATURECOUNTS_SCRIPT" "$EXPERIMENT_DIR" "$GENOME" > "$FEATURECOUNTS_LOG" 2>&1
+echo "run_featureCounts_batched.sh completed."
+
