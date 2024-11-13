e1b842f5ef87045b6128305ae1bda7eb8557f7e5
jsdearbo <jakedeaborn@gmail.com>
2024-11-13 13:01:15 -0500

    readability improvements

--- 5_star_loop.sh
+++ 5_star_loop.sh
@@ -9,19 +9,19 @@ if [ "$2" = "mouse" ]; then
 elif [ "$2" = "human" ]; then
   REF_GENOME=/bobross/STAR_Metadata/human/STAR
 else
-  echo "No reference genome set"
+  echo "Error: No reference genome set. Please specify 'mouse' or 'human' as the second argument."
+  exit 1
 fi
 
 echo "Reference Genome Path: $REF_GENOME"
 
 # Set directories
 INPUT_DIR="$1/fastq"
-OUTPUT_DIR="$1/bam" # output bam files
+OUTPUT_DIR="$1/bam"        # output bam files
 STAR_LOG_DIR="$1/star_logs" # STAR log output files
 
 # Create the output and STAR logs directories if they don't exist
-mkdir -p "$OUTPUT_DIR"
-mkdir -p "$STAR_LOG_DIR"
+mkdir -p "$OUTPUT_DIR" "$STAR_LOG_DIR"
 
 # Loop over the fastq files in the input directory
 for file in "$INPUT_DIR"/*_R1.fastq; do
@@ -31,6 +31,7 @@ for file in "$INPUT_DIR"/*_R1.fastq; do
   # Set the paths to the read 1 and read 2 files
   r1="$INPUT_DIR/${sample}_R1.fastq"
   r2="$INPUT_DIR/${sample}_R2.fastq"
+  output_bam="$OUTPUT_DIR/${sample}.bam"
 
   # Check if the input files exist
   if [ ! -f "$r1" ] || [ ! -f "$r2" ]; then
@@ -38,12 +39,12 @@ for file in "$INPUT_DIR"/*_R1.fastq; do
     continue
   fi
 
-  echo "r1: $r1"
-  echo "r2: $r2"
-  echo "sample: $sample"
+  echo "Processing sample: $sample"
+  echo "Read 1: $r1"
+  echo "Read 2: $r2"
 
-  # Run STAR alignment
-  if [ ! -f "$OUTPUT_DIR/${sample}.bam" ]; then
+  # Run STAR alignment if output BAM file does not already exist
+  if [ ! -f "$output_bam" ]; then
     $STAR \
       --runThreadN 16 \
       --genomeDir "$REF_GENOME" \
@@ -51,19 +52,18 @@ for file in "$INPUT_DIR"/*_R1.fastq; do
       --outFileNamePrefix "$OUTPUT_DIR/$sample" \
       --outSAMtype BAM SortedByCoordinate
 
+    # Check if STAR ran successfully
+    if [ $? -ne 0 ]; then
+      echo "Error: STAR alignment failed for sample $sample"
+      continue
+    fi
+
+    # Rename output BAM to simplify filename
+    mv "$OUTPUT_DIR/${sample}Aligned.sortedByCoord.out.bam" "$output_bam"
   else
     echo "Skipping $sample as an alignment already exists in the output directory."
   fi
 
-  # Check the exit status of STAR and handle errors if needed
-  if [ $? -ne 0 ]; then
-    echo "Error: STAR alignment failed for sample $sample"
-    # Add additional error handling code here if needed
-  fi
-
-  # Simplify output bam filenames
-  mv "$OUTPUT_DIR/${sample}"Aligned.sortedByCoord.out.bam "$OUTPUT_DIR/${sample}.bam"
-  # Move all files with '.out' or '.tab' extensions to the STAR log directory
-  mv "$OUTPUT_DIR"/*.out "$OUTPUT_DIR"/*.tab "$STAR_LOG_DIR"
-
+  # Move STAR output logs to the STAR log directory
+  mv "$OUTPUT_DIR/${sample}"*.out "$OUTPUT_DIR/${sample}"*.tab "$STAR_LOG_DIR" 2>/dev/null
 done
